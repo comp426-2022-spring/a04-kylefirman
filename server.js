3655fd369f965dfecc2aa2944ee772df100768d5
kylefirman <kpfirman@live.unc.edu>
2022-04-26 14:37:02 -0400

    test databse.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const morgan = require('morgan');
 const args = require('minimist')(process.argv.slice(2));
 
 // Require exports from database.js
-const db = require('./database.js');
+const db = require('./backup db.js');
 
 // Require Express.js
 const express = require('express');
